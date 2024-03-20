b99d678ec8d60f2f0dafca1215bedb7c7d533c03
Mila Chabassier <mili25cha@gmail.com>
2024-03-20 19:44:33 +0100

    affichage de chiffres

--- subject/script.js
+++ subject/script.js
@@ -5,3 +5,20 @@
 // ===============
 
 //Projet de mila chabassier
+
+const calculator = document.querySelector(".calculator")
+const keys = calculator.querySelector(".calculator__keys")
+
+keys.addEventListener("click", e => {
+ if (e.target.matches("button")) {
+   // Do something
+ }
+})
+
+
+const key = e.target
+const action = key.dataset.action
+
+if (!action) {
+    console.log('number key!')
+  }
